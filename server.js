66c026102742d6c4a2c347d03038328e077b0748
CrayonMuncher6000 <wadieibrahimcama@gmail.com>
2025-06-26 17:41:26 +0800

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const scholarshipRoutes = require('./routes/scholarships');
+const scholarshipRoutes = require('./server/routes/scholarships');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
