5aac3d5d679151296b6ccecb57cd539572d8c85a
CrayonMuncher6000 <wadieibrahimcama@gmail.com>
2025-06-26 17:44:32 +0800

    servalls

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const scholarshipRoutes = require('./server/routes/scholarships');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/scholarships', scholarshipRoutes);
+app.use(express.static('src'));
+app.use('/Css', express.static('Css'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/src/LogIn.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
